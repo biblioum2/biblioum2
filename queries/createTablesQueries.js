ed2cd29c06981044b672f86270355fa8306c012c
Chris <biblioacc117umte@gmail.com>
2024-06-21 01:05:34 -0600

    creacion crear tablas queries

---
+++ queries/createTablesQueries.js
@@ -0,0 +1,61 @@
+const pool = require('../config/database');
+
+const createTableUsers = `
+CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,  -- Guardar la contraseña hasheada
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`;
+
+const createTableAuthors = `
+CREATE TABLE IF NOT EXISTS authors (
+    author_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    biography TEXT
+);
+`;
+
+const createTableCategories = `
+CREATE TABLE IF NOT EXISTS categories (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+`;
+
+const createTableBooks = `
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT NOT NULL,
+    category_id INT NOT NULL,
+    publication_date DATE,
+    isbn VARCHAR(20) UNIQUE,
+    summary TEXT,
+    cover_image_filename VARCHAR(255),  -- Almacena el nombre del archivo de la portada
+    FOREIGN KEY (author_id) REFERENCES authors(author_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+`;
+
+const createTableFavorites = `
+CREATE TABLE favorites (
+    favorite_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    book_id INT NOT NULL,
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    UNIQUE (user_id, book_id)  -- Asegura que un usuario no pueda marcar el mismo libro como favorito más de una vez
+);
+`;
+
+module.exports = {
+    createTableUsers,
+    createTableAuthors,
+    createTableCategories,
+    createTableBooks,
+    createTableFavorites
+};

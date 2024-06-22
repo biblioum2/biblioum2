2fe0d0851c0e669101874f947323afd4d8b1a360
Chris <biblioacc117umte@gmail.com>
2024-06-21 22:23:23 -0600

    Se agrego obtencion de libros

--- queries/getData.js
+++ queries/getData.js
@@ -1,2 +1,23 @@
 const pool = require('../config/database');
 
+const getBooks = async () => {
+    const query = `
+        SELECT books.book_id, books.title, authors.name AS author, categories.name AS category, books.publication_date, books.isbn, books.summary, books.cover_image_filename
+        FROM books
+        JOIN authors ON books.author_id = authors.author_id
+        JOIN categories ON books.category_id = categories.category_id;
+    `;
+
+    try {
+        const res = await pool.query(query);
+        console.log(res.rows);
+        return res;
+    } catch (error) {
+        console.log('Error al obtener los datos', error);
+    }
+};
+
+getBooks();
+module.export = {
+    getBooks
+};
--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { getBooks } = require('../queries/getData');
 
 // Ruta para la página principal
-router.get('/', (req, res) => {
-  res.render('main', { title: 'Página de Inicio' });
+router.get('/', async (req, res) => {
+  try {
+    const books = await getBooks();
+    res.render('main', { title: 'Página de Inicio', books });
+  } catch (error) {
+    console.log(`Error al consultar`, error);
+    res.status(500).send('Error al obtener los libros');
+  }
 });
 
 // Otras rutas básicas pueden ir aquí
